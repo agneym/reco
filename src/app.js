10f776928d7b11a9f8bcd3906b1c7372e080e3af
Agney <agney.menon@bigbinary.com>
2020-04-30 00:20:26 +0530

    Convert to mp4 using ffmpeg

--- src/app.js
+++ src/app.js
@@ -1,12 +1,88 @@
-const fs = require("fs");
-const ffmpeg = require("ffmpeg.js/ffmpeg-mp4.js");
+const worker = new Worker("node_modules/ffmpeg.js/ffmpeg-worker-mp4.js");
 
-const testData = new Uint8Array(fs.readFileSync("src/test.webm"));
-// Encode test video to VP8.
-const result = ffmpeg({
-  MEMFS: [{name: "test.webm", data: testData}],
-  arguments: ["-y", "-i", "test.webm", "-c:v", "copy", "out.mp4"],
-});
-// Write out.webm to disk.
-const out = result.MEMFS[0];
-fs.writeFileSync(out.name, Buffer(out.data));
+worker.onmessage = function(e) {
+  const msg = e.data;
+  switch (msg.type) {
+  case "ready":
+    worker.postMessage({type: "run", arguments: ["-version"]});
+    break;
+  case "stdout":
+    console.log(msg.data);
+    break;
+  case "stderr":
+    console.log(msg.data);
+    break;
+  case "done":
+    console.log(msg.data);
+    const out = msg.data.MEMFS[0];
+    if(out) {
+      const blob = new Blob([out.data], {
+        type: 'video/mp4'
+      });
+      const url = window.URL.createObjectURL(blob);
+      const link = document.getElementById('recording-link');
+      link.href = url;
+      link.click();
+    }
+    break;
+  case "error": {
+    console.log(msg);
+    break;
+  }
+  default:
+    console.log("something happened");
+  }
+};
+
+async function convert(testData) {
+  const buffer = await testData.arrayBuffer();
+  worker.postMessage({
+    type: "run",
+    MEMFS: [{name: "test.webm", data: buffer }],
+    arguments: ["-y", "-i", "test.webm", "-c:v", "copy", "out.mp4"],
+  });
+}
+
+const chunks = [];
+let mediaRecorder = null;
+let stream = null;
+
+function stopCapture() {
+  if(mediaRecorder && stream) {
+    mediaRecorder.stop();
+    stream.getTracks().forEach(track => track.stop());
+    setTimeout(() => {
+      const recording = new Blob(chunks, { type: "video/webm" });
+      convert(recording);
+    }, 1000);
+  }
+}
+
+async function startRecording() {
+  try {
+    stream = await navigator.mediaDevices.getDisplayMedia(
+      {
+        video: {
+          cursor: "always",
+        },
+        audio: false,
+      }
+    );
+    stream.addEventListener("inactive", stopCapture);
+    mediaRecorder = new MediaRecorder(stream, {
+      mimeType: "video/webm",
+    });
+    mediaRecorder.start();
+    mediaRecorder.addEventListener("dataavailable", event => {
+      const data = event.data;
+      if (data && data.size > 0) {
+        chunks.push(data);
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+const startBtn = document.getElementById('startBtn');
+startBtn.addEventListener('click', startRecording);
