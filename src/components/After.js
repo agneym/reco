04a45b17ff1e655ec6352f0d0b5ceca9dff143e1
Agney <agney.menon@bigbinary.com>
2020-05-01 12:13:17 +0530

    after renders the video

--- src/components/After.js
+++ src/components/After.js
@@ -1,7 +1,14 @@
 import { html } from "htm/preact";
+import { useMemo } from "preact/hooks";
 
-function Intro() {
-  return html` <button>This is the After</button> `;
+/**
+ * @component Component to be rendered after recording.
+ */
+function After({ recording }) {
+  const webmUrl = useMemo(() => {
+    return URL.createObjectURL(recording);
+  }, [recording]);
+  return html` <video controls autoplay src=${webmUrl} /> `;
 }
 
-export default Intro;
+export default After;
