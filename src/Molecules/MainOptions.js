dd2b9fab3e3def9b8fa68a952372bd6dac9cd2bb
Agney <agney.menon@bigbinary.com>
2020-05-06 14:03:21 +0530

    longer caption

--- src/Molecules/MainOptions.js
+++ src/Molecules/MainOptions.js
@@ -5,9 +5,9 @@ import StartBtn from "./StartBtn.js";
 function MainOptions({ setPrimary }) {
   return html`
   <h1
-    class="text-2xl uppercase tracking-wide text-center font-normal m-6"
+    class="text-md text-gray-600 text-center font-normal m-6"
   >
-    Start Recording
+    Choose an option from below to start recording
   </h1>
   <${StartBtn} onClick=${() => setPrimary("screen")}>Screen</${StartBtn}>
   <${StartBtn} onClick=${() => setPrimary("camera")}>Camera</${StartBtn}>
