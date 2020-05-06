e7ac4424fa4a901cb5fd799777b9323189e12627
Agney <agney.menon@bigbinary.com>
2020-05-07 03:01:42 +0530

    remove webm in use

--- src/components/After.js
+++ src/components/After.js
@@ -7,14 +7,11 @@ import useConverter from "../hooks/useConverter.js";
  * @component Component to be rendered after recording.
  */
 function After({ recording, restart }) {
-  const { webmUrl, mp4Url } = useConverter(recording);
+  const { mp4Url } = useConverter(recording);
 
   return html`
     <div class="text-center">
-      <video class="max-w-xl m-6" controls autoplay src=${webmUrl} />
-      <${DownloadBtn} href=${webmUrl}>
-        Download WebM
-      </${DownloadBtn}>
+      <video class="max-w-xl m-6" controls autoplay src=${mp4Url} />
       <${DownloadBtn} href=${mp4Url}>
         Download MP4
       </${DownloadBtn}>
