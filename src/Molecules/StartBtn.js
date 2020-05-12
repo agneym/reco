4ae314da818bce9a476a962248a6b174df767bff
Agney <agney.menon@bigbinary.com>
2020-05-12 11:01:36 +0530

    add a dots background

--- src/Molecules/StartBtn.js
+++ src/Molecules/StartBtn.js
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 function StartBtn({ imgUrl, onClick, children }) {
   return html`
     <${motion.button}
-      class="mt-5 mx-8 px-12 py-6 shadow-lg hover:shadow-xl focus:shadow-outline"
+      class="mt-5 mx-8 px-12 py-6 shadow-lg hover:shadow-xl focus:shadow-outline bg-white"
       animate
       onClick=${onClick}
     >
