2d5c100a4eaa5c326ff2b194444e9852a7eb1584
Agney <agney.menon@bigbinary.com>
2020-05-16 03:57:35 +0530

    remove reference to path

--- build-utils/webpack.prod.js
+++ build-utils/webpack.prod.js
@@ -1,4 +1,3 @@
-const path = require("path");
 const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
